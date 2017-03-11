9cb20a98f14010c9d40a0f6e8cc3af5ed3991466
D1ADEV <nicolasmanichon@yahoo.fr>
2017-03-11 17:26:58 +0100

    Updated, now working again with the new website

--- PwnVoltaire/Form1.Designer.cs
+++ PwnVoltaire/Form1.Designer.cs
@@ -28,50 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.webKitBrowser1 = new WebKit.WebKitBrowser();
             this.SuspendLayout();
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Location = new System.Drawing.Point(20, 20);
-            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(30, 31);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(1786, 811);
-            this.webBrowser1.TabIndex = 0;
             // 
             // richTextBox1
             // 
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(20, 863);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 779);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(1434, 178);
+            this.richTextBox1.Size = new System.Drawing.Size(1251, 117);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1464, 863);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(1270, 779);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(342, 180);
+            this.button1.Size = new System.Drawing.Size(228, 117);
             this.button1.TabIndex = 2;
             this.button1.Text = "Check";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // webKitBrowser1
+            // 
+            this.webKitBrowser1.BackColor = System.Drawing.Color.White;
+            this.webKitBrowser1.Location = new System.Drawing.Point(13, 12);
+            this.webKitBrowser1.Name = "webKitBrowser1";
+            this.webKitBrowser1.Size = new System.Drawing.Size(1485, 761);
+            this.webKitBrowser1.TabIndex = 3;
+            this.webKitBrowser1.Url = null;
+            this.webKitBrowser1.Load += new System.EventHandler(this.webKitBrowser1_Load);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1824, 1085);
+            this.ClientSize = new System.Drawing.Size(1510, 908);
+            this.Controls.Add(this.webKitBrowser1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.webBrowser1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "PwnVoltaire";
             this.ResumeLayout(false);
@@ -79,10 +77,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
+        private WebKit.WebKitBrowser webKitBrowser1;
     }
 }
 
